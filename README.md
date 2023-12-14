81d0eeddd3b319e4194005acd7ab788aa5ac7e2a
Hashers.Club <hashers.club@gmail.com>
2023-12-14 11:36:49 +0100

    Initial commit - Forked from CashTab and adapted for Ergon (XRG) and SLP tokens

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Calory
-Calory : A web wallet for Ergon (XRG) and SLP tokens
+
+Calory is a web-based wallet application designed for the secure management of Ergon (XRG) and SLP tokens. It is intuitive and user-friendly, catering to both novice and seasoned cryptocurrency enthusiasts.
+
+## Features
+
+- **Ergon (XRG) Support**: Send, receive, and manage XRG with ease.
+- **SLP Tokens**: Full management of Simple Ledger Protocol tokens.
+- **User-Friendly Interface**: A modern and easy-to-navigate UI/UX.
+- **Cross-platform**: Access it from any web enabled device.
+
+## Getting Started
+
+These instructions will guide you through setting up Calory on your local machine for development and testing purposes.
+
+### Prerequisites
+
+Ensure you have the latest versions of Node.js and npm installed. Visit the [Node.js website](https://nodejs.org/) for download and installation instructions.
+
+### Installation
+
+1. Clone the Calory repository:
+
+   ```bash
+   git clone https://github.com/ErgonSurfer/Calory.git```
+
+2. Change directory to Calory:
+
+   ```bash
+   cd Calory```
+
+3. Install the dependencies:
+
+   ```bash
+   npm i```
+
+4. Start the application with:
+
+   ```bash
+   npm start```
+
+
+The web wallet will be accessible at http://localhost:3000.
+
+
+### Acknowledgments
+
+A heartfelt thank you to the developers of CashTab for creating the foundation upon which Calory is built.
+The Ergon community and all the contributors for their continuous support and contributions.
+
+
+
---
+++ src/assets/styles/theme.js
@@ -0,0 +1,77 @@
+export const theme = {
+    ergonBlue: '#0074C2', // A modern, bright blue
+    ergonPurple: '#9C27B0',
+    darkBlue: '#1E3A8A',
+    contrast: '#FFFFFF',
+    backgroundImage: `url("/cashtab_bg.png") no-repeat fixed`,
+    backgroundSize: 'cover', // Ensure the background covers the entire screen
+    backgroundColor: '#F5F5F7',
+    walletBackground: '#0E4D92',
+    walletInfoContainer: '#0E4D92',
+    footerBackground: '#0E4D92',
+    navActive: '#0074C2',
+    encryptionRed: '#E53935',
+    genesisGreen: '#00C853',
+    aliasGreen: '#0FFF50',
+    receivedMessage: 'rgba(0, 116, 194, 0.2)',
+    sentMessage: 'rgba(255, 255, 255, 0.1)',
+    lightWhite: 'rgba(255, 255, 255, 0.6)',
+    lightGrey: 'hsla(0, 0%, 49.8%, 0.65);',
+    dropdownText: '#000000',
+    shadow: 'rgba(0, 0, 0, 0.2)', // Softer shadow for subtlety
+    switchButtonActiveText: '#FFFFFF',
+    advancedCollapse: {
+        background: '#0E4D92',
+        color: '#FFFFFF',
+        icon: '#FFFFFF',
+        expandedBackground: 'rgba(0,0,0,0.2)',
+    },
+    forms: {
+        error: '#FF21D0',
+        border: '#17171f',
+        text: '#FFFFFF',
+        addonBackground: '#0E4D92',
+        addonForeground: '#FFFFFF',
+        selectionBackground: '#0E4D92',
+        placeholder: 'rgba(255,255,255,255)',
+        highlightBox: '#00ABE7',
+    },
+    icons: { outlined: '#FFFFFF' },
+    settings: {
+        delete: '#CD0BC3',
+        background: 'rgba(0,0,0,0.4)',
+    },
+    qr: {
+        background: '#FFFFFF',
+    },
+    buttons: {
+        primary: {
+            backgroundImage: 'linear-gradient(270deg, #0074C2 0%, #1E3A8A 100%)',
+            color: '#FFFFFF',
+            hoverShadow: '0px 4px 14px -3px rgba(0, 0, 0, 0.25)',
+        },
+        secondary: {
+            background: '#4b67e1',
+            color: '#FFFFFF',
+            hoverShadow: '0px 3px 10px -5px rgba(0, 0, 0, 0.75)',
+        },
+        alternateSecondary: {
+            background: '#255173',
+            color: '#FFFFFF',
+            hoverShadow: '0px 3px 10px -5px rgba(0, 0, 0, 0.75)',
+        },
+        styledLink: '#FFFFFF',
+    },
+    collapses: {
+        background: '#0E4D92',
+        expandedBackground: '#26415a',
+        border: '#17171f',
+        color: '#FFFFFF',
+    },
+    modal: {
+        background: '#0E4D92',
+        border: '#17171f',
+        color: '#FFFFFF',
+        buttonBackground: '#26415a',
+    },
+};
